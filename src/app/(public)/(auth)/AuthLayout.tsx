fb2902a64fb412319f6737197737ef7c405f195d
kaynanDev <kaynanldev@gmail.com>
2026-07-01 21:12:03 -0300

    feat: components ForgoutPassword and ResetPassword

--- src/app/(public)/(auth)/AuthLayout.tsx
+++ src/app/(public)/(auth)/AuthLayout.tsx
@@ -1,6 +1,7 @@
 import Logo from "@/src/components/ui/Logo";
+import SpinLoader from "@/src/components/ui/SpinLoader";
 import Image from "next/image";
-
+import { Suspense } from "react";
 interface AuthLayoutProps {
   title: string;
   subtitle?: string;
@@ -8,33 +9,41 @@ interface AuthLayoutProps {
   wide?: boolean;
 }
 
-export default function AuthLayout({ title, children, wide, subtitle }: AuthLayoutProps) {
+export default function AuthLayout({
+  title,
+  children,
+  wide,
+  subtitle,
+}: AuthLayoutProps) {
   return (
-    <div className="min-h-screen flex bg-deep-black">
-      {/* Lado esquerdo */}
-      <div className="relative hidden lg:block w-1/2">
-        <Image
-          src="/assets/backlogin.png"
-          alt="Background"
-          fill
-          className="object-cover object-left"
-          priority
-        />
-      </div>
+    <Suspense fallback={<SpinLoader />}>
+      <div className="min-h-screen flex bg-deep-black">
+        <div className="relative hidden lg:block w-1/2">
+          <Image
+            src="/assets/backAuth.png"
+            alt="Background"
+            fill
+            className="object-cover object-left"
+            priority
+          />
+        </div>
 
-      <div className="flex flex-1 items-center justify-center px-8">
-        <div className={`w-full ${wide ? "max-w-3xl" : "max-w-md"} bg-deep-black rounded-xl p-8 shadow-md`}>
-          <Logo className="mx-auto mb-6" />
+        <div className="flex flex-1 items-center justify-center px-8">
+          <div
+            className={`w-full ${wide ? "max-w-3xl" : "max-w-md"} bg-deep-black rounded-xl p-8 shadow-md`}
+          >
+            <Logo className="mx-auto mb-6" />
 
-          <h2 className="text-2xl text-center font-bold text-white">{title}</h2>
+            <h2 className="text-2xl text-center font-bold text-white">
+              {title}
+            </h2>
 
-          <p className="mt-2 mb-8 text-center text-gray-300">
-            {subtitle ?? "Entre para explorar os lançamentos do momento."}
-          </p>
+            <p className="mt-2 mb-8 text-center text-gray-300">{subtitle}</p>
 
-          {children}
+            {children}
+          </div>
         </div>
       </div>
-    </div>
+    </Suspense>
   );
 }
