953e50009d41d9bf37915c6dde4af70299875682
kaynanDev <kaynanldev@gmail.com>
2026-07-10 19:44:24 -0300

    fix: Change to the registration request

--- src/actions/registerActions.ts
+++ src/actions/registerActions.ts
@@ -35,7 +35,7 @@ export async function RegisterUser(
     const validData = parse(registerSchema, rawData);
 
     const response = await fetch(
-      process.env.NEXT_PUBLIC_API_URL + "/api/register",
+      process.env.NEXT_PUBLIC_API_URL + "/register",
       {
         method: "POST",
         body: JSON.stringify(validData),
