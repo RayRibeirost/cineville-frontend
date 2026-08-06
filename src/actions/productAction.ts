a97900d447ed102e8a1ca481f4c6f4b85a86ab95
kaynanDev <kaynanldev@gmail.com>
2026-08-06 20:33:50 -0300

    fix: Correction to the action's return value

---
+++ src/actions/productAction.ts
@@ -0,0 +1,54 @@
+"use server";
+
+import { cookies } from "next/headers";
+
+export interface ProductResponse {
+  _id: string;
+  name: string;
+  category: "BEBIDAS" | "COMIDAS" | "COMBOS";
+  size?: string;
+  maxLimit: number;
+  quantity: number;
+  price: number;
+  isAvailable: boolean;
+  imageUrl: string;
+}
+
+export async function getBomboniere() {
+  try {
+    const token = (await cookies()).get("auth_token")?.value;
+
+    const response = await fetch(
+      `${process.env.NEXT_PUBLIC_API_URL}/products/availables`,
+      {
+        method: "GET",
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+        cache: "no-store",
+      },
+    );
+
+    const data = await response.json();
+
+    if (!response.ok) {
+      return {
+        success: false,
+        error: data.message ?? "Erro ao buscar produtos.",
+      };
+    }
+
+    return {
+      success: true,
+      products: data.data as ProductResponse[],
+    };
+  } catch (error) {
+    return {
+      success: false,
+      error:
+        error instanceof Error
+          ? error.message
+          : "Erro interno ao buscar produtos.",
+    };
+  }
+}
